d35e431678211d442e3bb8507732d6a144d47c14
charl <charlesvader2@gmail.com>
2024-05-01 14:57:58 +0200

    basics

--- UNO.py
+++ UNO.py
@@ -2,12 +2,49 @@ import discord
 import random
 from discord.ext import commands
 
+bot = commands.Bot(command_prefix="!", intents = discord.Intents.all())
+
+TOK_FILE = "token.txt"
+
+
+card_type = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9"
+]
+
+card_color = [
+    "Red",
+    "Yellow",
+    "Green",
+    "Blue"
+]
 
 
 
 
 
+class cards:
 
 
 
 
+def get_token():
+    tokfile = open(TOK_FILE, 'r')
+    token = tokfile.read()
+    tokfile.close()
+    return token
+
+
+def pull_cards():
+    card_type =
+    card_color =
+    card = cards(card_type, card_color).card_name()
+    return card
